a22d0fe60bfc954762908cfe8852e393ff8a6872
Hamid Siddiqi <sidd0066@algonquinlive.com>
2022-05-16 16:44:43 -0400

    Done today

--- main.js
+++ main.js
@@ -1,12 +1,26 @@
 const app = Vue.createApp({
     data: function(){
         return {
+            cart: 0,
             product: 'Socks',
             image: '/images/socks_blue.jpg',
             inventory: 8,
-            details: ['50% cotton', '30% wool', '20% polyester']
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                { id: 2234, color: 'green', image: '/images/socks_green.jpg'},
+                { id: 2235, color: 'blue', image: '/images/socks_blue.jpg'},
+            ]
+        }
+    },
+    methods: {
+        addToCart(){
+            this.cart += 1
+        },
+        updateImage(variantImage){
+
+            this.image = variantImage
         }
     }
 })
 
-// Figure out image issue
+// Continue on lesson #7 Class & Style Bending
