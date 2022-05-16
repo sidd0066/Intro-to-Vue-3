71f5035bd6ca99a903d97a73dcd4edb3eee7c6d2
Hamid Siddiqi <sidd0066@algonquinlive.com>
2022-05-16 15:48:45 -0400

    adding files

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data: function(){
+        return {
+            product: 'Socks',
+            image: 'images/socks_green.jpg'
+        }
+    }
+})
