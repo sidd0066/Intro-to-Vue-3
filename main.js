3043761516cd993bdd6935aee7f515501a355011
Hamid Siddiqi <sidd0066@algonquinlive.com>
2022-05-16 16:19:47 -0400

    fixing image and html

--- main.js
+++ main.js
@@ -2,7 +2,11 @@ const app = Vue.createApp({
     data: function(){
         return {
             product: 'Socks',
-            image: 'images/socks_green.jpg'
+            image: '/images/socks_blue.jpg',
+            inventory: 8,
+            details: ['50% cotton', '30% wool', '20% polyester']
         }
     }
 })
+
+// Figure out image issue
